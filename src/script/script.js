dfa236752df80e5cfaf6f378ef1360f3653a08d7
Gifarfajar321 <gifarifajarm174@gmail.com>
2024-09-18 19:09:32 +0700

    menambahkan darkmode

--- src/script/script.js
+++ src/script/script.js
@@ -2,19 +2,58 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
   if (window.pageYOffset > fixNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
   }
 };
 
+// Hamburger
 const hamburger = document.querySelector("#hamburger");
-const navMenu = document.querySelector("#nav-menu")
-
+const navMenu = document.querySelector("#nav-menu");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("hamburger-active");
-  navMenu.classList.toggle("hidden")
+  navMenu.classList.toggle("hidden");
 });
+
+// Hamburger no focus
+window.addEventListener("click", (e) => {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// Dark mode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", () => {
+  // darkToggle.checked ? html.classList.add("dark"): html.classList.remove("dark")
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// pindahkan posisi toggle sesuai mode
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = true;
+}
